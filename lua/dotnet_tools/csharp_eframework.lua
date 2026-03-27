05b2003186162a624e5c4d6437dd5778160a6539
KonradChyrzynski <konrad.chyrzynskii@gmail.com>
2026-03-27 18:33:16 +0100

    "WIP-csharp_eframework

--- lua/dotnet_tools/csharp_eframework.lua
+++ lua/dotnet_tools/csharp_eframework.lua
@@ -26,34 +26,58 @@ EF CORE PARAMETERS EXPLAINED:
 --TODO: Pick projects with telescope
 local M = {}
 
-function pick_db_context_project(csproj_files)
+-- Function to run a .NET EF command with custom variables
+function M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
+	-- Construct the full command string
+	local full_command = string.format(
+		"dotnet ef %s --project %s --startup-project %s -o %s",
+		command,
+		project_path,
+		startup_project_path,
+		output_path
+	)
+
+	-- Execute the command
+	vim.cmd("!" .. full_command)
+end
+
+-- Example usage: Running a migration command
+function M.add_migration(migration_name, project_path, startup_project_path, output_path)
+    --TODO: Get migration name from user input
+	local command = string.format("migrations add %s", migration_name)
+	M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
+end
+
+-- Example usage: Running a script generation command
+function M.generate_migration_script(from_migration, to_migration, project_path, startup_project_path, output_path)
+    -- TODO: Add search for migration names
+	local command = string.format("migrations script %s %s", from_migration, to_migration)
+	M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
+end
+
+local function pick_db_context_project(csproj_files, main_csproj, command)
 	local other_projects = vim.tbl_filter(function(p)
 		return p ~= main_csproj
 	end, csproj_files)
 
 	pickers
 		.new({}, {
-			prompt_title = "Select Projects to Reference",
+			prompt_title = "Select db context project",
 			finder = finders.new_table({ results = other_projects }),
 			sorter = conf.generic_sorter({}),
 			attach_mappings = function(prompt_bufnr, map)
-				local picker = action_state.get_current_picker(prompt_bufnr)
 
 				actions.select_default:replace(function()
-					local selected = picker:get_multi_selection()
-
-					local refs = {}
-					if #selected == 0 then
-						local sel = action_state.get_selected_entry()
-						table.insert(refs, sel[1])
-					else
-						for _, entry in ipairs(selected) do
-							table.insert(refs, entry[1])
-						end
-					end
-
-					run_dotnet_add(main_csproj, refs)
+					local selection = action_state.get_selected_entry(prompt_bufnr)
 					actions.close(prompt_bufnr)
+
+					local db_context_project = vim.fn.fnamemodify(selection[1], ":p:h")
+                    local startup_project_directory = vim.fn.fnamemodify(main_csproj, ":p:h")
+
+                    if command == "add migration" then
+                        M.add_migration()
+                    end
+
 				end)
 
 				map("i", "<CR>", actions.select_default)
@@ -77,7 +101,7 @@ local function pick_startup_project_for_reference(csproj_files)
 					local selection = action_state.get_selected_entry(prompt_bufnr)
 					actions.close(prompt_bufnr)
 					local main = selection[1]
-					pick_projects_to_reference(csproj_files, main)
+					pick_db_context_project(csproj_files, main)
 				end)
 				return true
 			end,
@@ -87,33 +111,6 @@ end
 
 function AddTemp()
     reference.search_for_csproj_files(pick_startup_project_for_reference)
-end
-
--- Function to run a .NET EF command with custom variables
-function M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
-	-- Construct the full command string
-	local full_command = string.format(
-		"dotnet ef %s --project %s --startup-project %s -o %s",
-		command,
-		project_path,
-		startup_project_path,
-		output_path
-	)
-
-	-- Execute the command
-	vim.cmd("!" .. full_command)
-end
-
--- Example usage: Running a migration command
-function M.add_migration(migration_name, project_path, startup_project_path, output_path)
-	local command = string.format("migrations add %s", migration_name)
-	M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
-end
-
--- Example usage: Running a script generation command
-function M.generate_migration_script(from_migration, to_migration, project_path, startup_project_path, output_path)
-	local command = string.format("migrations script %s %s", from_migration, to_migration)
-	M.run_dotnet_ef_command(command, project_path, startup_project_path, output_path)
 end
 
 return M
